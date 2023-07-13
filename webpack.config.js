b8383b73e4e3ed70f4af257cb882afb7ce76db8d
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-07-14 02:43:13 +0300

    Added css loaders in modules

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,8 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+  modules: {
+    rules: [{ test: /\.css$/i, use: ['style-loader', 'css-loader'] }]
+  },
   plugins: [new html({ template: './src/index.html' })]
 };
