dda50809e92371e0d5f918946cc5a69cfe87f444
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-07-14 02:47:13 +0300

    Fixed typo in module property name

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
-  modules: {
+  module: {
     rules: [{ test: /\.css$/i, use: ['style-loader', 'css-loader'] }]
   },
   plugins: [new html({ template: './src/index.html' })]
