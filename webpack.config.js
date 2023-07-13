bb860bcd79825065bfcb29e4d09f22303739ca6a
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-07-14 01:44:01 +0300

    Implemented html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
+const html = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
+    clean: true,
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [new html({ title: 'WebPack Demo Exercise' })]
 };
