bb0167d11785e010fbe9a14564c1d2b8324d0785
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-07-14 03:10:26 +0300

    Renamed source & distribution directories

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 const html = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './source/index.js',
   output: {
     clean: true,
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'build')
   },
   module: {
     rules: [{ test: /\.css$/i, use: ['style-loader', 'css-loader'] }]
   },
-  plugins: [new html({ template: './src/index.html' })]
+  plugins: [new html({ template: './source/index.html' })]
 };
